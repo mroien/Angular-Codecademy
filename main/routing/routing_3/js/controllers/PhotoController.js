bad7c386d30343b990886ea6d797ba526ab4f871
mroien <oien.tim@gmail.com>
2015-09-10 22:03:41 -0500

    updated PhotoController

--- main/routing/routing_3/js/controllers/PhotoController.js
+++ main/routing/routing_3/js/controllers/PhotoController.js
@@ -1,5 +1,6 @@
 app.controller('PhotoController', ['$scope', 'photos', '$routeParams', function($scope, photos, $routeParams) {
   photos.success(function(data) {
+  	// $routeParams used to grad id from the URL
     $scope.detail = data[$routeParams.id];
   });
 }]);
